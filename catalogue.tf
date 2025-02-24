93c77eb5f73a2e4c2103ec2fc901ebde5ab4cb79
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-24 08:20:02 +0530

    Training Code

--- catalogue.tf
+++ catalogue.tf
@@ -1,20 +1,19 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0ea2a448676b70f53"]
 
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z01662431H5LL60AVTC0E"
   name    = "frontend-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
-
 
--- mongodb.tf
+++ mongodb.tf
@@ -1,20 +1,21 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "mongodb" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0ea2a448676b70f53"]
 
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongodb" {
   zone_id = "Z01662431H5LL60AVTC0E"
-  name    = "frontend-dev"
+  name    = "mongodb-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.mongodb.private_ip]
 }
+
 
 
