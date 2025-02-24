c66e7eb36d268b9723a96bdf0eb3aac1ebc88867
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-24 08:14:38 +0530

    Training Code

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0ea2a448676b70f53"]
 
   tags = {
     Name = "frontend"
