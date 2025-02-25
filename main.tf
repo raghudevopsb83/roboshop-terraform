cf9d47edb7c92ea233f44eb13e68fcdb246b59d5
Raghu K <raghuk.vit@gmail.com>
2025-02-25 08:12:51 +0530

    RoboShop Code

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
-    Name = var.instances[each.key]
+    Name = each.key
   }
 }
 
@@ -13,7 +13,7 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "record" {
   for_each = var.instances
   zone_id = var.zone_id
-  name    = "${var.instances[each.key]}-${var.env}"
+  name    = "${each.key}-${var.env}"
   type    = "A"
   ttl     = 10
   records = [aws_instance.instance[each.key].private_ip]
