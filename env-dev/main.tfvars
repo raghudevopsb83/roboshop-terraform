fc162bf2f8261e2df72fc6c81f36054b40b24d80
Raghu K <raghuk.vit@gmail.com>
2025-02-25 08:48:33 +0530

    RoboShop Code

---
+++ env-dev/main.tfvars
@@ -0,0 +1,19 @@
+instances = {
+  frontend = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+  catalogue = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+  mongodb = {
+    ami_id        = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+}
+
+zone_id = "Z01662431H5LL60AVTC0E"
+vpc_security_group_ids = ["sg-0ea2a448676b70f53"]
+env = "dev"
+
--- variables.tf
+++ variables.tf
@@ -1,28 +1,6 @@
-variable "vpc_security_group_ids" {
-  default = ["sg-0ea2a448676b70f53"]
-}
+variable "vpc_security_group_ids" {}
+variable "instances" {}
+variable "zone_id" {}
+variable "env" {}
 
-variable "instances" {
-  default = {
-    frontend = {
-      ami_id        = "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      ami_id        = "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-    }
-    mongodb = {
-      ami_id        = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-  }
-}
 
-variable "zone_id" {
-  default = "Z01662431H5LL60AVTC0E"
-}
-
-variable "env" {
-  default = "dev"
-}
