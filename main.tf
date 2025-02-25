ecfe2b35d0dae6990bf59024a8a68670082144d4
Raghu K <raghuk.vit@gmail.com>
2025-02-25 08:42:43 +0530

    RoboShop Code

--- main.tf
+++ main.tf
@@ -1,37 +1,13 @@
-resource "aws_instance" "instance" {
+module "ec2" {
   for_each = var.instances
-  ami           = each.value["ami_id"]
-  instance_type = each.value["instance_type"]
+  source   = "./modules/ec2"
+
+  ami_id                 = each.value["ami_id"]
+  env                    = var.env
+  instance_type          = each.value["instance_type"]
+  name                   = each.key
   vpc_security_group_ids = var.vpc_security_group_ids
-
-  tags = {
-    Name = each.key
-  }
+  zone_id                = var.zone_id
 }
 
 
-resource "aws_route53_record" "record" {
-  for_each = var.instances
-  zone_id = var.zone_id
-  name    = "${each.key}-${var.env}"
-  type    = "A"
-  ttl     = 10
-  records = [aws_instance.instance[each.key].private_ip]
-}
-
-# resource "null_resource" "catalogue" {
-#   provisioner "remote-exec" {
-#
-#     connection {
-#       type     = "ssh"
-#       user     = "ec2-user"
-#       password = "DevOps321"
-#       host     = aws_instance.catalogue.private_ip
-#     }
-#
-#     inline = [
-#       "sudo pip3.11 install ansible",
-#       "ansible-pull -i localhost, -U https://github.com/raghudevopsb83/roboshop-ansible roboshop.yml -e component_name=catalogue -e env-dev",
-#     ]
-#   }
-# }
---
+++ modules/ec2/main.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = var.name
+  }
+}
+
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-${var.env}"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance.private_ip]
+}
+
+# resource "null_resource" "catalogue" {
+#   provisioner "remote-exec" {
+#
+#     connection {
+#       type     = "ssh"
+#       user     = "ec2-user"
+#       password = "DevOps321"
+#       host     = aws_instance.catalogue.private_ip
+#     }
+#
+#     inline = [
+#       "sudo pip3.11 install ansible",
+#       "ansible-pull -i localhost, -U https://github.com/raghudevopsb83/roboshop-ansible roboshop.yml -e component_name=catalogue -e env-dev",
+#     ]
+#   }
+# }
+
+
