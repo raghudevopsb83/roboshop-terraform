acd64bf24686b77630ada1849a64970d4c12d7f3
Raghu K <raghuk.vit@gmail.com>
2025-03-10 08:41:12 +0530

    RoboShop Code

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -17,7 +17,7 @@ resource "aws_eks_cluster" "main" {
 resource "aws_eks_node_group" "main" {
   for_each        = var.node_groups
   cluster_name    = aws_eks_cluster.main.name
-  node_group_name = each.name
+  node_group_name = each.key
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnets
   instance_types  = each.value["instance_types"]
