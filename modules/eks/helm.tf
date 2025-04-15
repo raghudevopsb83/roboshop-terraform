d9623110f081478ce87bd184ad278a6e6e89e062
Raghu K <raghuk.vit@gmail.com>
2025-04-15 08:03:11 +0530

    RoboShop Code

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -21,6 +21,11 @@ resource "helm_release" "kube-prometheus-stack" {
   name       = "kube-prom-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
+
+
+  values = [
+    file("${path.module}/helm-config/prom-stack-${var.env}.yml")
+  ]
 }
 
 
