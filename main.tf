7cbeaa649653f83f6c8c590235859aeee1a7d6e7
Raghu K <raghuk.vit@gmail.com>
2025-04-28 07:55:19 +0530

    Training Code

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ module "ec2" {
 
 
 
-# module "eks" {
-#   for_each    = var.eks
-#   source      = "./modules/eks"
-#   env         = var.env
-#   eks_version = each.value["eks_version"]
-#   subnets     = each.value["subnets"]
-#   node_groups = each.value["node_groups"]
-#   addons      = each.value["addons"]
-#   access      = each.value["access"]
-# }
+module "eks" {
+  for_each    = var.eks
+  source      = "./modules/eks"
+  env         = var.env
+  eks_version = each.value["eks_version"]
+  subnets     = each.value["subnets"]
+  node_groups = each.value["node_groups"]
+  addons      = each.value["addons"]
+  access      = each.value["access"]
+}
