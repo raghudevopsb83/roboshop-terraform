88326a543308e917588b853d4ff67b8d765adf84
Raghu K <raghuk.vit@gmail.com>
2025-05-27 08:51:13 +0530

    RoboShop Code

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,6 +1,6 @@
 output "subnets" {
 #   value = [for k, v in var.subnets : aws_subnet.main[v.group].id]
-  value = {for k,v in var.subnets : v.group => [for k, v in var.subnets : aws_subnet.main[k].id]}
+  value = [for k,v in aws_subnet.main : v.id ]
 }
 
 
